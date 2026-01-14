be22ba88b3cf84a75f425bc08b5ce226766e4cc0
Nayandeep <nayandeepsen@gmail.com>
2026-01-14 12:00:00 +0530

    added customer class to explain function returns

---
+++ src/com/kodewala/arraylist/customers/Driver.java
@@ -0,0 +1,19 @@
+package com.kodewala.arraylist.customers;
+
+import java.util.ArrayList;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		CustomerDetails c = new CustomerDetails();
+
+		ArrayList<String> custDetails = c.cusDetails();
+
+		for (String cus : custDetails) {
+			if (cus.endsWith("ra")) {
+				System.out.println(cus);
+			}
+		}
+	}
+
+}
