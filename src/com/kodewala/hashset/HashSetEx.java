7b9c227d0aac7182409c6f701703dec603d2a907
Nayandeep <nayandeepsen@gmail.com>
2026-01-20 12:00:00 +0530

    added: hashset package & classes

---
+++ src/com/kodewala/hashset/HashSetEx.java
@@ -0,0 +1,23 @@
+package com.kodewala.hashset;
+
+import java.util.HashSet;
+
+public class HashSetEx {
+
+	public static void main(String[] args) {
+		HashSet<String> h = new HashSet<String>();
+		h.add("Blr");
+		h.add("Mum");
+		h.add("Mum");
+		h.add("Chn");
+		h.add(null);
+		h.add(null);
+
+		String str = "Bdadasdsasd";
+		h.add(str);
+		System.out.println(h.contains("Blr"));
+
+		System.out.println(h);
+	}
+
+}
---
+++ src/com/kodewala/hashset/Person.java
@@ -0,0 +1,37 @@
+package com.kodewala.hashset;
+
+import java.util.HashSet;
+
+public class Person {
+
+	private String name;
+
+	Person(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) { // If you override equals method & not hashcode then it will result in two
+										// different objects in two different buckets
+		Person p = (Person) obj;
+		return this.name.equals(p.name);
+	}
+
+	@Override
+	public int hashCode() { // If you override hash but not equals then it results in hash collision because
+							// its returning same hash but objects are not equal
+		return this.name.hashCode();
+	}
+
+	public static void main(String[] args) {
+
+		Person p1 = new Person("Nayandeep");
+		Person p2 = new Person("Nayandeep");
+		HashSet<Person> h = new HashSet<Person>();
+		h.add(p1);
+		h.add(p2);
+
+		System.out.println(h.size());
+	}
+
+}
