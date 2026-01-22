8c8f49cf39c1aa90e1504e8b6240a4718663090b
Nayandeep <nayandeepsen@gmail.com>
2026-01-22 12:00:00 +0530

    added: linked hashset

---
+++ src/com/kodewala/hashset/linkedhashset/LinkedHashSetEx.java
@@ -0,0 +1,17 @@
+package com.kodewala.hashset.linkedhashset;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEx {
+	public static void main(String[] args) {
+		// maintains order using linked list
+		LinkedHashSet<String> h = new LinkedHashSet<String>();
+		h.add("hello");
+		h.add("hi");
+		h.add("how");
+		h.add("are");
+		h.add("you");
+		h.add("hello");
+		System.out.println(h);
+	}
+}
