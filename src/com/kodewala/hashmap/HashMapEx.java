2169223bc5d5d3874d2f2f7083789d6ca4c3f08c
Nayandeep <nayandeepsen@gmail.com>
2026-01-23 12:00:00 +0530

    added: hashmap

---
+++ src/com/kodewala/hashmap/HashMapEx.java
@@ -0,0 +1,21 @@
+package com.kodewala.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		Map<String, String> statesAndCapitals = new HashMap<String, String>();
+		statesAndCapitals.put("KA", "BLR");
+		statesAndCapitals.put("MH", "MUM");
+		statesAndCapitals.put("BH", "PT");
+		
+		for(<String,String> sc :statesAndCapitals) {
+		
+		}
+
+		System.out.println(statesAndCapitals);
+	}
+
+}
